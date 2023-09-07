20c580fa823051680b3e16482c8618ea33a1dd7f
Ben Prize Bar-On <prizeben@gmail.com>
2023-09-07 10:23:06 +0300

    Detect only people

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ from ultralytics.engine.results import Results
 model = YOLO('yolov8n.pt')
 
 # Predict with the model
-results: list[Results] = model.predict('https://ultralytics.com/images/bus.jpg')
+results: list[Results] = model.predict(
+    'https://ultralytics.com/images/bus.jpg',
+    classes=0)
 
 for result in results:
     im_array = result.plot()  # plot a BGR numpy array of predictions
