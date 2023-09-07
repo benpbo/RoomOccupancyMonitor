f1f2cf18cc8a2a30afc3e5ada0c660979b4910d8
Ben Prize Bar-On <prizeben@gmail.com>
2023-09-07 13:57:45 +0300

    Factor video capture prediction to separate function

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import sys
+from typing import Iterable
 
 import cv2
 from PIL import Image
 from ultralytics import YOLO
+from ultralytics.engine.model import Model
 from ultralytics.engine.results import Results
 
 MINIMUM_CONFIDENCE = 0.5
+
+def predict_capture(model: Model, capture: cv2.VideoCapture) -> Iterable[Results]:
+    while capture.isOpened():
+        success, frame = capture.read()
+        if not success:
+            break
+
+        results: list[Results] = model.predict(
+            frame, conf=MINIMUM_CONFIDENCE, classes=0)
+        yield results[0]
 
 # Load the model
 model = YOLO('yolov8n.pt')
@@ -13,25 +25,16 @@ model = YOLO('yolov8n.pt')
 # Predict with the model
 _, video_path = sys.argv
 capture = cv2.VideoCapture(video_path)
-while capture.isOpened():
-    success, frame = capture.read()
-    if success:
-        results: list[Results] = model.predict(
-            frame, conf=MINIMUM_CONFIDENCE, classes=0)
-        result = results[0]
+for result in predict_capture(model, capture):
+    detection_count = len(result.boxes.data)
+    print(f'Detected {detection_count} persons')
 
-        detection_count = len(result.boxes.data)
-        print(f'Detected {detection_count} persons')
-    
-        # Display the annotated frame
-        annotated_frame = result.plot()
-        cv2.imshow("YOLOv8 Inference", annotated_frame)
+    # Display the annotated frame
+    annotated_frame = result.plot()
+    cv2.imshow("YOLOv8 Inference", annotated_frame)
 
-        # Break the loop if 'q' is pressed
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-    else:
-        # Break the loop if the end of the video is reached
+    # Break the loop if 'q' is pressed
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
     
 capture.release()
